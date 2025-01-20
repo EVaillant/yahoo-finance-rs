43a4884a153ee1b8620d603303aeaeb3b70daf43
Etienne Vaillant <vaillant.etienne@gmail.com>
2025-01-20 21:21:41 +0100

    Basic draft

---
+++ examples/fetch_chart.rs
@@ -0,0 +1,78 @@
+use chrono::Local;
+use clap::Parser;
+use env_logger::Builder;
+use log::info;
+use log::LevelFilter;
+use std::io::Write;
+
+use yahoo_finance_api::Interval;
+use yahoo_finance_api::YahooBuilder;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// ticket yahoo
+    #[clap(short, long, value_parser)]
+    ticker: String,
+
+    // period yyyymmdd-yyyymmdd
+    #[clap(short, long, value_parser = parse_period)]
+    period: Option<(chrono::NaiveDate, chrono::NaiveDate)>,
+
+    // interval
+    #[clap(short, long, value_parser = parse_interval)]
+    interval: Option<Interval>,
+}
+
+fn parse_period(arg: &str) -> Result<(chrono::NaiveDate, chrono::NaiveDate), clap::Error> {
+    let period: Vec<&str> = arg.split('-').collect();
+    if period.len() != 2 {
+        Err(clap::Error::new(clap::error::ErrorKind::ValueValidation))
+    } else {
+        let begin = chrono::NaiveDate::parse_from_str(period[0], "%Y%m%d")
+            .map_err(|_| clap::Error::new(clap::error::ErrorKind::ValueValidation))?;
+        let end = chrono::NaiveDate::parse_from_str(period[1], "%Y%m%d")
+            .map_err(|_| clap::Error::new(clap::error::ErrorKind::ValueValidation))?;
+        Ok((begin, end))
+    }
+}
+
+fn parse_interval(arg: &str) -> Result<Interval, clap::Error> {
+    match arg {
+        "1d" => Ok(Interval::Day1),
+        _ => Err(clap::Error::new(clap::error::ErrorKind::ValueValidation)),
+    }
+}
+
+fn main() {
+    //
+    // cli arg
+    let args = Args::parse();
+
+    //
+    // logger
+    Builder::new()
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} [{}] - {}",
+                Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .filter_level(LevelFilter::Info)
+        .parse_default_env()
+        .init();
+
+    let mut builder = YahooBuilder::new();
+    builder.set_ticker(args.ticker);
+    if let Some((begin, end)) = args.period {
+        builder.set_period(begin, end);
+    }
+    if let Some(interval) = args.interval {
+        builder.set_interval(interval);
+    }
+    let result = builder.request_chart().expect("request failed");
+    info!("{:?}", result);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,135 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use log::debug;
+use reqwest::blocking::Client;
+use reqwest::header::{HeaderMap, HeaderValue};
+use serde::Deserialize;
+
+#[derive(Copy, Clone, Debug)]
+pub enum Interval {
+    Day1,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl std::fmt::Display for Interval {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Interval::Day1 => write!(f, "1d"),
+        }
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, Deserialize)]
+pub struct YahooResult {}
+
+#[derive(Clone, Debug)]
+pub enum Error {
+    Initialize(String),
+    Request(String),
+    Json(String),
+}
+
+pub struct YahooBuilder {
+    ticker: Option<String>,
+    period_begin: Option<chrono::NaiveDate>,
+    period_end: Option<chrono::NaiveDate>,
+    interval: Option<Interval>,
+}
+
+impl YahooBuilder {
+    pub fn new() -> YahooBuilder {
+        Self {
+            ticker: None,
+            period_begin: None,
+            period_end: None,
+            interval: None,
+        }
+    }
+
+    pub fn set_ticker<T: Into<String>>(&mut self, ticker: T) -> &mut Self {
+        self.ticker = Some(ticker.into());
+        self
+    }
+
+    pub fn set_period<T1: Into<chrono::NaiveDate>, T2: Into<chrono::NaiveDate>>(
+        &mut self,
+        begin: T1,
+        end: T2,
+    ) -> &mut Self {
+        self.period_begin = Some(begin.into());
+        self.period_end = Some(end.into());
+        self
+    }
+
+    pub fn set_interval<T: Into<Interval>>(&mut self, interval: T) -> &mut Self {
+        self.interval = Some(interval.into());
+        self
+    }
+
+    pub fn request_chart(&self) -> Result<YahooResult, Error> {
+        let url = self.make_request_chart_url()?;
+        debug!("request chart on '{url}'");
+
+        let output = self
+            .make_http_client()?
+            .get(&url)
+            .send()
+            .map_err(|error| Error::Request(format!("failed to request '{url}' because:{error}")))?
+            .text()
+            .map_err(|error| {
+                Error::Request(format!("failed to read body '{url}' because:{error}"))
+            })?;
+        debug!("request result: {output}");
+
+        let request: YahooResult = serde_json::from_reader(output.as_bytes()).map_err(|error| {
+            Error::Json(format!("failed to parse json because {error} : {output} "))
+        })?;
+        Ok(request)
+    }
+
+    fn make_request_chart_url(&self) -> Result<String, Error> {
+        let ticker = self.ticker.as_ref().ok_or(Error::Initialize(
+            "ticker argument is mandatoty".to_string(),
+        ))?;
+        let period1 = self.period_begin.map_or(String::new(), |date| {
+            format!(
+                "period1={}",
+                date.and_hms_opt(0, 0, 0).unwrap().and_utc().timestamp()
+            )
+        });
+        let period2 = self.period_end.map_or(String::new(), |date| {
+            format!(
+                "period2={}",
+                date.and_hms_opt(0, 0, 0).unwrap().and_utc().timestamp()
+            )
+        });
+        let interval = self
+            .interval
+            .as_ref()
+            .map_or(String::new(), |interval| interval.to_string());
+
+        let prefix = format!(
+            "https://query1.finance.yahoo.com/v8/finance/chart/{ticker}?{period1}{period2}{interval}"
+        );
+        Ok(prefix)
+    }
+
+    fn make_http_client(&self) -> Result<Client, Error> {
+        let mut headers = HeaderMap::new();
+        headers.insert("Connection", HeaderValue::from_static("keep-alive"));
+        headers.insert("Expires", HeaderValue::from_static("-1"));
+        headers.insert("Upgrade-Insecure-Requests", HeaderValue::from_static("1"));
+        headers.insert("User-Agent", HeaderValue::from_static("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"));
+
+        let client = Client::builder()
+            .cookie_store(true)
+            .default_headers(headers)
+            .build()
+            .map_err(|error| Error::Initialize(format!("failed to init reqwest : {error}")))?;
+
+        Ok(client)
+    }
+}
+
+impl Default for YahooBuilder {
+    fn default() -> Self {
+        Self::new()
     }
 }
